c8e4c8cebf8fb83bad898d58b520c98f9236f835
Lukas Biewald <lukeab@gmail.com>
2018-01-29 19:49:29 -0700

    cleanup

--- keras-cnn-fashion/train.py
+++ keras-cnn-fashion/train.py
@@ -4,7 +4,7 @@ Trains a simple cnn on the fashion mnist dataset.
 Deigned to show how to do a simple wandb integration with keras.
 """
 
-from keras.datasets import mnist
+from keras.datasets import fashion_mnist
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 from keras.utils import np_utils
@@ -16,8 +16,16 @@ from wandb.wandb_keras import WandbKerasCallback
 
 run = wandb.init()
 config = run.config
+config.dropout = 0.2
+config.hidden_layer_size = 128
+config.layer_1_size  = 16
+config.layer_2_size = 32
+config.learn_rate = 0.01
+config.decay = 1e-6
+config.momentum = 0.9
+config.epochs = 25
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
 img_width=28
 img_height=28
@@ -36,25 +44,23 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=config.learn_rate, decay=config.decay, momentum=config.momentum,
+                            nesterov=True)
 
 # build model
-
 model = Sequential()
-
-model.add(Conv2D(config.layer_1_size, (5, 5), input_shape=(img_width, img_height,1), activation='relu'))
+model.add(Conv2D(config.layer_1_size, (5, 5), activation='relu',
+                            input_shape=(img_width, img_height,1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Conv2D(config.layer_2_size, (5, 5), input_shape=(img_width, img_height,1), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.2))
+model.add(Conv2D(config.layer_2_size, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(config.dropout))
 model.add(Flatten())
-model.add(Dense(8, activation='relu'))
+model.add(Dense(config.hidden_layer_size, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-#model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=config.epochs, callbacks=[WandbKerasCallback()])
 model.fit(X_train, y_train,  validation_data=(X_test, y_test), epochs=config.epochs,
-    callbacks=[WandbKerasCallback(),
-                TensorBoard(write_images=True, write_grads=True, histogram_freq=1)])
+    callbacks=[WandbKerasCallback()])
 
-model.save("convnet.h5")
+model.save("cnn.h5")
