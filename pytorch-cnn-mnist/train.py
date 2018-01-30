b74bc8caceeede74c7a1b9240c2654d2ee634f05
Lukas Biewald <lukeab@gmail.com>
2018-01-29 18:57:52 -0700

    Added pytorch example

---
+++ pytorch-cnn-mnist/train.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+import torch.nn.functional as F
+from fashion_data import fashion
+
+import wandb
+import os
+
+run = wandb.init()
+
+run.config.dropout = 0.5
+run.config.channels_one = 16
+run.config.channels_two = 32
+
+normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
+                                     std=[x/255.0 for x in [63.0, 62.1, 66.7]])
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.1307,), (0.3081,))])
+
+train_dataset = fashion(root='./data',
+                            train=True,
+                            transform=transform,
+                            download=True
+                           )
+
+test_dataset = fashion(root='./data',
+                            train=False,
+                            transform=transform,
+                           )
+
+
+label_names = ["T-shirt/top",
+"Trouser",
+"Pullover",
+"Dress",
+"Coat",
+"Sandal",
+"Shirt",
+"Sneaker",
+"Bag",
+"Boot"]
+
+batch_size = 100
+n_iters = 5500
+num_epochs = n_iters / (len(train_dataset) / batch_size)
+num_epochs = int(num_epochs)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+
+
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+
+        # Convolution 1
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=run.config.channels_one, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        # Max pool 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # Convolution 2
+        self.cnn2 = nn.Conv2d(in_channels=run.config.channels_one, out_channels=run.config.channels_two, kernel_size=5, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+
+        # Max pool 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.dropout = nn.Dropout(p=run.config.dropout)
+
+        # Fully connected 1 (readout)
+        self.fc1 = nn.Linear(run.config.channels_two*4*4, 10)
+
+    def forward(self, x):
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+
+        # Max pool 1
+        out = self.maxpool1(out)
+
+        # Convolution 2
+        out = self.cnn2(out)
+        out = self.relu2(out)
+
+        # Max pool 2
+        out = self.maxpool2(out)
+
+        # Resize
+        # Original size: (100, 32, 7, 7)
+        # out.size(0): 100
+        # New out size: (100, 32*7*7)
+        out = out.view(out.size(0), -1)
+        out = self.dropout(out)
+        # Linear function (readout)
+        out = self.fc1(out)
+
+        return out
+
+model = CNNModel()
+criterion = nn.CrossEntropyLoss()
+run.config.learning_rate = 0.001
+
+optimizer = torch.optim.Adam(model.parameters(), lr=run.config.learning_rate)
+
+iter = 0
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+
+        images = Variable(images)
+        labels = Variable(labels)
+
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
+
+        # Forward pass to get output/logits
+        outputs = model(images)
+
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+
+        # Updating parameters
+        optimizer.step()
+
+        iter += 1
+
+        if iter % 100 == 0:
+            # Calculate Accuracy
+            correct = 0
+            correct_arr = [0] * 10
+            total = 0
+            total_arr = [0] * 10
+
+            # Iterate through test dataset
+            for images, labels in test_loader:
+                images = Variable(images)
+
+                # Forward pass only to get logits/output
+                outputs = model(images)
+
+                # Get predictions from the maximum value
+                _, predicted = torch.max(outputs.data, 1)
+
+                # Total number of labels
+                total += labels.size(0)
+
+
+                correct += (predicted == labels).sum()
+                for label in range(10):
+                    correct_arr[label] += (((predicted == labels) + (labels == label)) == 2).sum()
+                    total_arr[label] += (labels == label).sum()
+
+
+            accuracy = 100 * correct / total
+
+            metrics = {'accuracy': accuracy, 'loss': loss.data[0]}
+            for label in range(10):
+                metrics['Accuracy ' + label_names[label]] = correct_arr[label] / total_arr[label]
+            run.history.add(metrics)
+            run.summary.update(metrics)
+
+
+            # Print Loss
+            print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.data[0], accuracy))
