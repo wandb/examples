c32a7a16dd59c91fc5aeeae371ce6b9238766d83
Shawn Lewis <shlewis@gmail.com>
2019-12-10 11:53:02 -0800

    Matplotlib example

---
+++ matplotlib/mpl.py
@@ -0,0 +1,55 @@
+# Example showing how to log matplotlib plots, which get
+# displayed as plotly plots in the W&B UI
+
+import numpy as np
+import matplotlib.pyplot as plt
+import wandb
+
+wandb.init()
+
+#### Scatter plot with histograms
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# the random data
+x = np.random.randn(1000)
+y = np.random.randn(1000)
+
+# definitions for the axes
+left, width = 0.1, 0.65
+bottom, height = 0.1, 0.65
+spacing = 0.005
+
+
+rect_scatter = [left, bottom, width, height]
+rect_histx = [left, bottom + height + spacing, width, 0.2]
+rect_histy = [left + width + spacing, bottom, 0.2, height]
+
+# start with a rectangular Figure
+plt.figure(figsize=(8, 8))
+
+ax_scatter = plt.axes(rect_scatter)
+ax_scatter.tick_params(direction='in', top=True, right=True)
+ax_histx = plt.axes(rect_histx)
+ax_histx.tick_params(direction='in', labelbottom=False)
+ax_histy = plt.axes(rect_histy)
+ax_histy.tick_params(direction='in', labelleft=False)
+
+# the scatter plot:
+ax_scatter.scatter(x, y)
+
+# now determine nice limits by hand:
+binwidth = 0.25
+lim = np.ceil(np.abs([x, y]).max() / binwidth) * binwidth
+ax_scatter.set_xlim((-lim, lim))
+ax_scatter.set_ylim((-lim, lim))
+
+bins = np.arange(-lim, lim + binwidth, binwidth)
+ax_histx.hist(x, bins=bins)
+ax_histy.hist(y, bins=bins, orientation='horizontal')
+
+ax_histx.set_xlim(ax_scatter.get_xlim())
+ax_histy.set_ylim(ax_scatter.get_ylim())
+
+wandb.log({'scatter-with-histos': plt})
