a55b03881bdd96dd85f6a512ae894c46fbd6c3ae
Lukas Biewald <lukas@wandb.com>
2020-04-29 14:23:49 -0700

    add xgboost regressor example

---
+++ xgboost-boston/train.py
@@ -0,0 +1,28 @@
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+from sklearn.datasets import load_boston
+import pandas as pd
+import numpy as np
+boston = load_boston()
+
+data = pd.DataFrame(boston.data)
+X, y = data.iloc[:,:-1],data.iloc[:,-1]
+data_dmatrix = xgb.DMatrix(data=X,label=y)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+xg_reg = xgb.XGBRegressor(objective ='reg:linear', 
+    colsample_bytree = 0.3, 
+    learning_rate = 0.1,
+    max_depth = 5, 
+    alpha = 10, 
+    n_estimators = 10)
+
+xg_reg.fit(X_train,y_train,callbacks=[wandb.xgboost.wandb_callback()])
+
+preds = xg_reg.predict(X_test)
+
+rmse = np.sqrt(mean_squared_error(y_test, preds))
+print("RMSE: %f" % (rmse))
