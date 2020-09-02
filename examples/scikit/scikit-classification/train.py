3f07615d62903855ac88aa965d158d4cc83b7f96
charlesfrye <charles@wandb.com>
2020-09-02 13:49:13 -0700

    closes #12 and formats for PEP8

--- examples/scikit/scikit-classification/train.py
+++ examples/scikit/scikit-classification/train.py
@@ -1,33 +1,34 @@
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-import pickle
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import wandb
+
 wandb.init(project="sklearn")
 
 # Load data
 wine_quality = pd.read_csv("wine.csv")
 y = wine_quality["quality"]
-X = wine_quality.drop(["quality"], axis = 1)
-feature_names=wine_quality.columns
+X = wine_quality.drop(["quality"], axis=1)
+feature_names = wine_quality.columns
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-labels = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+labels = ['one', 'two', 'three', 'four', 'five',
+          'six', 'seven', 'eight', 'nine', 'ten']
 
-# Train model, get predictions
+# Train model
 model = RandomForestClassifier()
 model.fit(X, y)
+
+# Get predictions
 y_pred = model.predict(X_test)
 y_probas = model.predict_proba(X_test)
 importances = model.feature_importances_
 indices = np.argsort(importances)[::-1]
 
 print(X_train.info())
-X_train = X_train[1:]
-X_test = X_test[1:]
+
 # Visualize model performance
-wandb.sklearn.plot_classifier(model, X_train, X_test, y_train, y_test, y_pred, y_probas, labels, False, 'RandomForest', feature_names)
+wandb.sklearn.plot_classifier(
+    model, X_train, X_test, y_train, y_test, y_pred, y_probas, labels,
+    is_binary=False, model_name='RandomForest', feature_names=feature_names)
