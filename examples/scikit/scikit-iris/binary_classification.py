d0959efa4e4f17bd43a79cbf9d20d1a3f0ec935e
Ayush Thakur <mein2work@gmail.com>
2020-09-03 23:26:23 +0530

    Update binary_classification.py

--- examples/scikit/scikit-iris/binary_classification.py
+++ examples/scikit/scikit-iris/binary_classification.py
@@ -1,13 +1,10 @@
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from numpy.random.mtrand import permutation
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils import shuffle
 import wandb
+
+# initialize wandb run
 wandb.init()
 
 # load data
@@ -17,19 +14,9 @@ y = iris.target
 y[y != 0] = 1
 
 # shuffle data
-random_idx = permutation(np.arange(len(y)))
-X = X[random_idx]
-y = y[random_idx]
+X, y = shuffle(X, y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 # create model
 model = RandomForestClassifier()
 wandb.sklearn.plot_learning_curve(model, X_test, y_test)
-wandb.sklearn.plot_class_balance(y_train, y_test)
-wandb.sklearn.plot_calibration_curve(X, y, RandomForestClassifier(), name="Random Forest")
-wandb.sklearn.plot_decision_boundaries(model, X, y)
-
-'''
-# Visualize model performance
-# wandb.sklearn.log(rf, X=X, y=y, X_test=X_test, y_test=y_test, labels=feature_names)
-'''
