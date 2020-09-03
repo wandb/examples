826d69e2f5eede523ac260a767a0b7c424cd97a3
Ayush Thakur <mein2work@gmail.com>
2020-09-03 23:20:41 +0530

    Update train.py

--- examples/scikit/scikit-clustering/train.py
+++ examples/scikit/scikit-clustering/train.py
@@ -4,8 +4,11 @@ from sklearn import datasets, cluster
 from sklearn.datasets import load_iris
 import numpy as np
 import wandb
+
+# initialize wandb run
 wandb.init()
 
+# load data
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
@@ -16,8 +19,6 @@ def get_label_ids(classes):
 labels = get_label_ids(y)
 
 kmeans = KMeans(n_clusters=4, random_state=1)
-dbscan = DBSCAN(eps=0.008, min_samples=2)
-agg = AgglomerativeClustering()
 cluster_labels = kmeans.fit_predict(X)
 
 # wandb.sklearn.plot_elbow_curve(kmeans, X)
