572201a941bb76d4a5aa14e4913bca497f7b1f06
charlesfrye <charles@wandb.com>
2020-11-06 13:26:27 -0800

    fixes resume, lints file

--- examples/keras/keras-cnn-fashion/train.py
+++ examples/keras/keras-cnn-fashion/train.py
@@ -2,47 +2,47 @@
 
 """
 Trains a simple cnn on the fashion mnist dataset.
-Deigned to show how to do a simple wandb integration with keras.
+Designed to show how to do a simple wandb integration with keras.
+Also demonstrates resumption of interrupted runs with
+    python train.py --resume
 """
+import sys
 
 from keras.datasets import fashion_mnist
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 from keras.utils import np_utils
 from keras.optimizers import SGD
-from keras.callbacks import TensorBoard
 
 import wandb
 from wandb.keras import WandbCallback
 
 
-defaults=dict(
-    dropout = 0.2,
-    hidden_layer_size = 128,
-    layer_1_size = 16,
-    layer_2_size = 32,
-    learn_rate = 0.01,
-    decay = 1e-6,
-    momentum = 0.9,
-    epochs = 27,
+defaults = dict(
+    dropout=0.2,
+    hidden_layer_size=128,
+    layer_1_size=16,
+    layer_2_size=32,
+    learn_rate=0.01,
+    decay=1e-6,
+    momentum=0.9,
+    epochs=27,
     )
 
-wandb.init(config=defaults, resume=True)
+resume = sys.argv[-1] == "--resume"
+wandb.init(config=defaults, resume=resume)
 config = wandb.config
 
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
-labels=["T-shirt/top","Trouser","Pullover","Dress","Coat",
-        "Sandal","Shirt","Sneaker","Bag","Ankle boot"]
+labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+          "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-img_width=28
-img_height=28
+img_width, img_height = 28, 28
 
-X_train = X_train.astype('float32')
-X_train /= 255.
-X_test = X_test.astype('float32')
-X_test /= 255.
+X_train = X_train.astype('float32') / 255.
+X_test = X_test.astype('float32') / 255.
 
-#reshape input data
+# reshape input data -- add channel dimension
 X_train = X_train.reshape(X_train.shape[0], img_width, img_height, 1)
 X_test = X_test.reshape(X_test.shape[0], img_width, img_height, 1)
 
@@ -52,17 +52,17 @@ y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
 sgd = SGD(lr=config.learn_rate, decay=config.decay, momentum=config.momentum,
-                            nesterov=True)
+          nesterov=True)
 
 # build model
 if wandb.run.resumed:
     print("RESUMING")
     # restore the best model
-    model = keras.models.load_model(wandb.restore("model-best.h5").name)
+    model = load_model(wandb.restore("model-best.h5").name)
 else:
     model = Sequential()
     model.add(Conv2D(config.layer_1_size, (5, 5), activation='relu',
-                            input_shape=(img_width, img_height,1)))
+                     input_shape=(img_width, img_height, 1)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(config.layer_2_size, (5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -71,10 +71,12 @@ else:
     model.add(Dense(config.hidden_layer_size, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.compile(
+        loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(X_train, y_train,  validation_data=(X_test, y_test), epochs=config.epochs,
-    initial_epoch=wandb.run.step,
-    callbacks=[WandbCallback(data_type="image", labels=labels)])
+model.fit(X_train, y_train,  validation_data=(X_test, y_test),
+          epochs=config.epochs,
+          initial_epoch=wandb.run.step,  # for resumed runs
+          callbacks=[WandbCallback(data_type="image", labels=labels)])
 
 model.save("cnn.h5")
