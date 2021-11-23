938d6944cfa861062edb106df7cd367818c3d94e
Rohit Gupta <rohitgr1998@gmail.com>
2021-11-23 21:35:55 +0530

    update lightning example (#99)

--- examples/pytorch-lightning/mnist.py
+++ examples/pytorch-lightning/mnist.py
@@ -1,4 +1,4 @@
-"""pytorch-lightning example with W&B logging
+"""pytorch-lightning example with W&B logging.
 
 Based on this colab:
 https://colab.research.google.com/drive/1F_RNcHzTfFuQf-LeKvSlud6x7jXYkG31
@@ -9,31 +9,23 @@ import os
 import torch
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-import torchvision
-from torchvision.datasets import MNIST
 from torchvision import transforms
+from torchvision.datasets import MNIST
+
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 
-# workaround to fetch MNIST data
-import sys
-tv_version = torchvision.__version__
-print("torchvision version:", tv_version, file=sys.stderr)
-if tuple(map(lambda x: int(x), tv_version.split(".")[:2])) <= (0, 5):
-    url = "https://activeeon-public.s3.eu-west-2.amazonaws.com/datasets/MNIST.old.tar.gz"
-else:
-    url = "https://activeeon-public.s3.eu-west-2.amazonaws.com/datasets/MNIST.new.tar.gz"
-print("download:", url, file=sys.stderr)
-
-os.system("wget -O MNIST.tar.gz {}".format(url))
-os.system("tar -zxvf MNIST.tar.gz")
 
 class MNISTModel(pl.LightningModule):
-
     def __init__(self):
-        super(MNISTModel, self).__init__()
+        super().__init__()
         # not the best model...
         self.l1 = torch.nn.Linear(28 * 28, 10)
+
+    def prepare_data(self):
+        # download MNIST data only once
+        MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor())
+        MNIST(os.getcwd(), train=False, download=True, transform=transforms.ToTensor())
 
     def forward(self, x):
         # called with self(x)
@@ -44,32 +36,22 @@ class MNISTModel(pl.LightningModule):
         x, y = batch
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
-        tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
+        self.log("train_loss", loss, on_step=True, on_epoch=False)
+        return loss
 
     def validation_step(self, batch, batch_nb):
         # OPTIONAL
         x, y = batch
         y_hat = self(x)
-        return {'val_loss': F.cross_entropy(y_hat, y)}
-
-    def validation_epoch_end(self, outputs):
-        # OPTIONAL
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+        loss = F.cross_entropy(y_hat, y)
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
 
     def test_step(self, batch, batch_nb):
         # OPTIONAL
         x, y = batch
         y_hat = self(x)
-        return {'test_loss': F.cross_entropy(y_hat, y)}
-
-    def test_epoch_end(self, outputs):
-        # OPTIONAL
-        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        logs = {'test_loss': avg_loss}
-        return {'test_loss': avg_loss, 'log': logs, 'progress_bar': logs}
+        loss = F.cross_entropy(y_hat, y)
+        self.log("test_loss", loss, on_step=False, on_epoch=True)
 
     def configure_optimizers(self):
         # REQUIRED
@@ -79,21 +61,26 @@ class MNISTModel(pl.LightningModule):
 
     def train_dataloader(self):
         # REQUIRED
-        return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=32)
+        return DataLoader(
+            MNIST(os.getcwd(), train=True, download=False, transform=transforms.ToTensor()), batch_size=32
+        )
 
     def val_dataloader(self):
         # OPTIONAL
-        return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=32)
+        return DataLoader(
+            MNIST(os.getcwd(), train=True, download=False, transform=transforms.ToTensor()), batch_size=32
+        )
 
     def test_dataloader(self):
         # OPTIONAL
-        return DataLoader(MNIST(os.getcwd(), train=False, download=True, transform=transforms.ToTensor()), batch_size=32)
+        return DataLoader(
+            MNIST(os.getcwd(), train=True, download=False, transform=transforms.ToTensor()), batch_size=32
+        )
 
-train_loader = DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=32)
 
-#wandb_logger = WandbLogger(project='pytorchlightning', tags=['testtag'])
-wandb_logger = WandbLogger()
-
-mnist_model = MNISTModel()
-trainer = pl.Trainer(gpus=0, progress_bar_refresh_rate=20, max_epochs=2, logger=wandb_logger)    
-trainer.fit(mnist_model, train_loader)
+if __name__ == "__main__":
+    wandb_logger = WandbLogger()
+    mnist_model = MNISTModel()
+    trainer = pl.Trainer(gpus=0, max_epochs=2, logger=wandb_logger)
+    trainer.fit(mnist_model)
+    trainer.test(mnist_model)
