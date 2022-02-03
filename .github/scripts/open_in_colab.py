b577d71076f4c14f5d2007f6ca001a820373a46b
Thomas Capelle <tcapelle@pm.me>
2022-02-03 21:45:54 +0100

    add push

--- .github/scripts/open_in_colab.py
+++ .github/scripts/open_in_colab.py
@@ -5,6 +5,9 @@ from nb_helpers.utils import git_current_branch
 
 def create_comment():
     "On PR post a comment with links to open in colab for each changed nb"
+
+    title = "The following colabs where changed in this PR:\n"
+
     api = GhApi(owner='wandb', repo='examples', token=github_token())
     payload = context_github.event
     if 'workflow' in payload: issue = 1
@@ -20,10 +23,24 @@ def create_comment():
         url = get_colab_url(fname, git_current_branch(fname))
         return f"- [{_fname}]({url})\n"
 
-    colab_links = tuple(get_colab_md(f) for f in nb_files)
-    body = ("The following colabs where changed in this PR:\n",)+colab_links
-    body = "".join(body)
+    def search_comment(issue_number):
+        "Search for older message"
+        comments = api.issues.list_comments(issue_number)
+        return [c for c in comments if'colabs where changed' in c.body]
 
-    api.issues.create_comment(issue_number=issue, body=body)
+    if len(nb_files)>0:
+        colab_links = tuple(get_colab_md(f) for f in nb_files)
+        body = tuplify(title) + colab_links
+        body = "".join(body)
+
+        previous_comments = search_comment(issue)
+
+        if len(previous_comments)>0:
+            "We already have the comment"
+            api.issues.update_comment(issue, body)
+        else:
+            api.issues.create_comment(issue_number=issue, body=body)
+    
+
 
 create_comment()
