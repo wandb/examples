f3d85949f2d5a403fa6bb19e98b375f802166a23
Thomas Capelle <tcapelle@pm.me>
2022-02-04 13:37:09 +0100

    Update CI (#184)
    * use ghapi to get branch name
    
    * typo
    
    * print issue
    
    * print payload
    
    * use pr info
    
    * wrong repo =(
    
    * clean up

--- .github/scripts/open_in_colab.py
+++ .github/scripts/open_in_colab.py
@@ -1,38 +1,45 @@
 from fastcore.all import *
 from ghapi.all import *
-from nb_helpers.colab import get_colab_url
-from nb_helpers.utils import git_current_branch, is_nb
+from nb_helpers.utils import git_local_repo, is_nb
+
 
 def create_comment():
     "On PR post a comment with links to open in colab for each changed nb"
 
-    api = GhApi(owner='wandb', repo='examples', token=github_token())
+    api = GhApi(owner="wandb", repo="examples", token=github_token())
     payload = context_github.event
-    if 'workflow' in payload: issue = 1
+
+    if "workflow" in payload:
+        issue = 1
     else:
-        if payload.action != 'opened': return
-        issue = payload.number
-    
+        if payload.action != "opened":
+            return
+
+    issue = payload.number
+    pr = payload.pull_request
+    github_repo = pr.head.repo.full_name
+    branch = pr.head.ref
     pr_files = [Path(f.filename) for f in api.pulls.list_files(issue)]
-    
-    #filter nbs
+
+    # filter nbs
     nb_files = [f for f in pr_files if is_nb(f)]
 
-    def _get_colab_url2md(fname: Path) -> str:
+    def _get_colab_url2md(fname: Path, github_repo=github_repo, branch=branch) -> str:
         "Create colab links in md"
-        url = get_colab_url(fname, git_current_branch(fname))
-        return f"- [{fname}]({url})\n"
+        fname = fname.relative_to(git_local_repo(fname))
+        colab_url = f"https://colab.research.google.com/github/{github_repo}/blob/{branch}/{str(fname)}"
+        return f"- [{fname}]({colab_url})\n"
 
-    def _create_comment_body(nb_files: list[Path]) -> str:
+    def _create_comment_body(nb_files) -> str:
         "Creates a MD list of fnames with links to colab"
         title = "The following colabs where changed in this PR:\n"
         colab_links = tuple(_get_colab_url2md(f) for f in nb_files)
         body = tuplify(title) + colab_links
         return "".join(body)
-        
-    if len(nb_files)>0:
+
+    if len(nb_files) > 0:
         body = _create_comment_body(nb_files)
         print(f">> Creating comment on PR #{issue}")
         api.issues.create_comment(issue_number=issue, body=body)
-    
+
 create_comment()
