ee6534bd06e882e63ccf1b477f2de670b44818c0
Thomas Capelle <tcapelle@pm.me>
2022-02-04 10:14:15 +0100

    move colab to 1st cell (#183)
    * move colab to 1st cell
    
    * prepend colabs folder
    
    * clean up
    
    * refactor wf

--- .github/scripts/open_in_colab.py
+++ .github/scripts/open_in_colab.py
@@ -1,12 +1,10 @@
 from fastcore.all import *
 from ghapi.all import *
 from nb_helpers.colab import get_colab_url
-from nb_helpers.utils import git_current_branch
+from nb_helpers.utils import git_current_branch, is_nb
 
 def create_comment():
     "On PR post a comment with links to open in colab for each changed nb"
-
-    title = "The following colabs where changed in this PR:\n"
 
     api = GhApi(owner='wandb', repo='examples', token=github_token())
     payload = context_github.event
@@ -15,22 +13,26 @@ def create_comment():
         if payload.action != 'opened': return
         issue = payload.number
     
-    pr_files = [f.filename for f in api.pulls.list_files(issue)]
-    nb_files = [f for f in pr_files if ".ipynb" in f]
+    pr_files = [Path(f.filename) for f in api.pulls.list_files(issue)]
+    
+    #filter nbs
+    nb_files = [f for f in pr_files if is_nb(f)]
 
-    def get_colab_md(fname):
-        _fname = Path(fname).name
+    def _get_colab_url2md(fname: Path) -> str:
+        "Create colab links in md"
         url = get_colab_url(fname, git_current_branch(fname))
-        return f"- [{_fname}]({url})\n"
+        return f"- [{fname}]({url})\n"
 
-
-    if len(nb_files)>0:
-        colab_links = tuple(get_colab_md(f) for f in nb_files)
+    def _create_comment_body(nb_files: list[Path]) -> str:
+        "Creates a MD list of fnames with links to colab"
+        title = "The following colabs where changed in this PR:\n"
+        colab_links = tuple(_get_colab_url2md(f) for f in nb_files)
         body = tuplify(title) + colab_links
-        body = "".join(body)
+        return "".join(body)
+        
+    if len(nb_files)>0:
+        body = _create_comment_body(nb_files)
         print(f">> Creating comment on PR #{issue}")
         api.issues.create_comment(issue_number=issue, body=body)
     
-
-
 create_comment()
