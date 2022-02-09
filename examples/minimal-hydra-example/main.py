39a51cb424bb9a46407036e6ebb12cebd39343e8
Adrish Dey <adrish@wandb.com>
2022-02-09 20:51:53 +0530

    minor bug fixes. adding requirements

--- examples/minimal-hydra-example/main.py
+++ examples/minimal-hydra-example/main.py
@@ -7,14 +7,16 @@ import wandb.keras
 def get_model(model_cfg):
 
     cfg = model_cfg
+    ksize = cfg.kernel_size
+
     model = tf.keras.Sequential(
         [
             tf.keras.Input(shape=[32, 32, 3]),
-            tf.keras.layers.Conv2D(3, cfg.l1_size, activation=cfg.activation),
+            tf.keras.layers.Conv2D(cfg.l1_size, ksize, activation=cfg.activation),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Conv2D(cfg.l1_size, cfg.l2_size, activation=cfg.activation),
+            tf.keras.layers.Conv2D(cfg.l2_size, ksize, activation=cfg.activation),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Conv2D(cfg.l2_size, cfg.l3_size, activation=cfg.activation),
+            tf.keras.layers.Conv2D(cfg.l3_size, ksize, activation=cfg.activation),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(cfg.last_size),
             tf.keras.layers.Dense(10, activation="softmax"),
@@ -27,14 +29,16 @@ def get_model(model_cfg):
 def run_experiment(cfg: omegaconf.DictConfig) -> None:
 
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    y_train = tf.one_hot(y_train, depth=10)
-    y_test = tf.one_hot(y_test, depth=10)
+    y_train = tf.squeeze(tf.one_hot(y_train, depth=10))
+    y_test = tf.squeeze(tf.one_hot(y_test, depth=10))
 
     with wandb.init(**cfg.wandb.setup) as run:
       model = get_model(cfg.model)
 
-      optimizer = tf.optimizers.get(cfg.optimizer)
-      model.compile(loss="categorical_crossentropy", optimizer=cfg.model.optimizer)
+      optim_cfg = omegaconf.OmegaConf.to_container(cfg.optimizer)
+      optimizer = tf.optimizers.get(optim_cfg)
+      model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+      model.summary()
       model.fit(
           x_train,
           y_train,
