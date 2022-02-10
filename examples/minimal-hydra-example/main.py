25536c66039642179d0c0d2fedb83b6c029bac82
Adrish Dey <adrish@wandb.com>
2022-02-10 18:16:58 +0530

    Adding minimal hydra example (#192)
    * adding minimal hydra example
    
    * minor bug fixes. adding requirements
    
    * minor bug fixes. linting yaml files. adding pretty-print.
    
    * adding wandb_sweep.yaml
    
    * fixing minor bugs
    
    * Create README.md

---
+++ examples/minimal-hydra-example/main.py
@@ -0,0 +1,54 @@
+import pprint
+
+import hydra
+import omegaconf
+import tensorflow as tf
+import wandb.keras
+
+
+def get_model(model_cfg):
+
+    cfg = model_cfg
+    ksize = cfg.kernel_size
+
+    model = tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=[32, 32, 3]),
+            tf.keras.layers.Conv2D(cfg.l1_size, ksize, activation=cfg.activation),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Conv2D(cfg.l2_size, ksize, activation=cfg.activation),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Conv2D(cfg.l3_size, ksize, activation=cfg.activation),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(cfg.last_size),
+            tf.keras.layers.Dense(10, activation="softmax"),
+        ]
+    )
+    return model
+
+
+@hydra.main(config_path="configs/", config_name="defaults")
+def run_experiment(cfg: omegaconf.DictConfig) -> None:
+
+    pprint.pprint(cfg, indent=2)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    y_train = tf.squeeze(tf.one_hot(y_train, depth=10))
+    y_test = tf.squeeze(tf.one_hot(y_test, depth=10))
+
+    with wandb.init(**cfg.wandb.setup) as run:
+        model = get_model(cfg.model)
+
+        optim_cfg = omegaconf.OmegaConf.to_container(cfg.optimizer)
+        optimizer = tf.optimizers.get(optim_cfg)
+        model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+        model.summary()
+        model.fit(
+            x_train,
+            y_train,
+            validation_data=(x_test, y_test),
+            callbacks=[wandb.keras.WandbCallback()],
+        )
+
+
+if __name__ == "__main__":
+    run_experiment()
