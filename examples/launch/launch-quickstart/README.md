27f68a5f7b10d036792e6a87bf501e91edbec3af
Ben Sherman <bcanfieldsherman@gmail.com>
2022-08-10 18:27:08 -0700

    reverted to fmnist pytorch example (#294)

--- examples/launch/launch-quickstart/README.md
+++ examples/launch/launch-quickstart/README.md
@@ -5,7 +5,7 @@ installed. You will also want to make sure you have authenticated your `wandb`
 client with `wandb login` or by settings the `WANDB_API_KEY` environment variable.
 
 The `train.py` script in this directory will train a simple neural network to
-classify handwritten digits from the MNIST dataset. It will also log training 
+classify handwritten digits from the Fashion MNIST dataset. It will also log training 
 metrics and evaluation results to Weights & Biases.
 
 From within this directory (`examples/examples/launch/launch-quickstart`) run
@@ -18,8 +18,8 @@ to build the training script into an image named `launch-quickstart`. We can
 use the `wandb launch` command to run a container from that image with our
 `wandb` credentials automatically passed in from our host environment.
 
-Running `wandb launch -d launch-quickstart -p mnist` will start the job and send
-metrics to a new project called `mnist` in your Weights & Biases account.
+Running `wandb launch -d launch-quickstart -p fmnist` will start the job and send
+metrics to a new project called `fmnist` in your Weights & Biases account.
 
 To learn how you use the same tool to launch jobs into Kubernetes or Amazon 
 SageMaker, check out more of [our docs](https://docs.wandb.ai/guides/launch)!
--- examples/launch/launch-quickstart/train.py
+++ examples/launch/launch-quickstart/train.py
@@ -1,67 +1,97 @@
 import wandb
-import numpy as np
-import tensorflow as tf
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import FashionMNIST
 
-# Training hyperparameters
+class FashionCNN(nn.Module):
+  """Simple CNN for Fashion MNIST."""
+
+  def __init__(self):
+    super().__init__()
+    self.layer1 = nn.Sequential(
+        nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+        nn.BatchNorm2d(32),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2)
+    )
+    self.layer2 = nn.Sequential(
+        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+        nn.BatchNorm2d(64),
+        nn.ReLU(),
+        nn.MaxPool2d(2)
+    )    
+    self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+    self.drop = nn.Dropout2d(0.25)
+    self.fc2 = nn.Linear(in_features=600, out_features=120)
+    self.fc3 = nn.Linear(in_features=120, out_features=10) 
+  
+  def forward(self, x):
+    out = self.layer1(x)
+    out = self.layer2(out)
+    out = out.view(out.size(0), -1)
+    out = self.fc1(out)
+    out = self.drop(out)
+    out = self.fc2(out)
+    out = self.fc3(out)
+    return out
+
+
+# Training hyperparamters
 config = {
-  "layer_1": 512,
-  "activation_1": "relu",
-  "dropout": 0.25,
-  "layer_2": 10,
-  "activation_2": "softmax",
-  "optimizer": "sgd",
-  "loss": "sparse_categorical_crossentropy",
-  "metric": "accuracy",
-  "epoch": 10,
-  "batch_size": 256,
+  "learning_rate": 0.0001,
+  "batch_size": 32,
+  "epochs": 5,
 }
 
-TABLE_COLUMNS = ["image", "label", "prediction"]
-TABLE_COLUMNS.extend([f"score_{i}" for i in range(10)])
-
-def build_pred_table(dataset, logits):
-    data = [
-        [wandb.Image(dataset["x"][idx]), dataset["y"][idx], np.argmax(logits[idx]), *logits[idx]]
-        for idx in range(len(logits))
-    ]
-    table = wandb.Table(data=data, columns=TABLE_COLUMNS)
-    return table
-
-def build_model(config):
-  """Construct a simple neural network."""
-  model = tf.keras.models.Sequential(
-    [
-      tf.keras.layers.Flatten(input_shape=(28, 28)),
-      tf.keras.layers.Dense(config.layer_1, activation=config.activation_1),
-      tf.keras.layers.Dropout(config.dropout),
-      tf.keras.layers.Dense(config.layer_2, activation=config.activation_2),
-    ]
-  )
-  model.compile(optimizer=config.optimizer, loss=config.loss, metrics=[config.metric])
-  return model
-
+# Pass config into wandb.init
 with wandb.init(config=config) as run:
-    
-    # Build model and prepare data
-    model = build_model(run.config)
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-    model.fit(
-        x=x_train,
-        y=y_train,
-        epochs=run.config.epoch,
-        batch_size=run.config.batch_size,
-        validation_data=(x_test[:100], y_test[:100]),
-        callbacks=[wandb.keras.WandbCallback(log_model=True, log_evaluation=True)],
-    )
+  
+  # Training setup
+  config = run.config
+  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+  model = FashionCNN()
+  model.to(device)
+  train_dataset = FashionMNIST("./data/", download=True, train=True, transform=transforms.ToTensor())
+  train_loader = DataLoader(train_dataset, batch_size=config.batch_size, pin_memory=True)
+  error = nn.CrossEntropyLoss()
+  learning_rate = config.learning_rate
+  optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    x_test = x_test[100:300]
-    y_test = y_test[100:300]
-    logits = model.predict(x_test)
-    preds = np.argmax(logits, axis=1)
-    loss = tf.keras.losses.SparseCategoricalCrossentropy()(y_test, logits).numpy()
-    accuracy = tf.keras.metrics.Accuracy()(y_test, preds).numpy()
-    run.summary["test_loss"] = loss
-    run.summary["test_accuracy"] = accuracy
-    run.log({"predictions": build_pred_table(dict(x=x_test, y=y_test), logits)})
+  # Epoch loop
+  iter = 0
+  losses = []
+  for epoch in range(config.epochs):
+
+    # Iterate over batches of the data
+    for idx, (images, labels) in enumerate(train_loader):
+
+      iter += 1
+      print(iter)
+      images = images.to(device)
+      labels = labels.to(device)
+      outputs = model(images)
+      loss = error(outputs, labels)
+      losses.append(loss.item())
+
+      if iter % 25 == 1:
+        print("loggin")
+        run.log(
+          {
+            "train/loss": sum(losses)/len(losses),  # Log average loss
+            "train/losses": wandb.Histogram(losses)  # Log all losses
+          }
+        )
+        losses = []
+
+      if idx == 0:
+        table = wandb.Table(columns=["image", "label", "prediction"])
+        for im, lab, pred in zip(images, labels, outputs):
+          pred = torch.argmax(pred)
+          table.add_data(wandb.Image(im.cpu()), lab.item(), pred.item())
+        run.log({"train/predictions": table})
+
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
