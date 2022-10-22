9ca40e5f67ebc04222944d3ff3c3e3a221f6652d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 06:13:05 +0000

    Adding tarfile member sanitization to extractall()

--- examples/fastai/fastai-v1-examples/fastai1-simpsons/train_sweep.py
+++ examples/fastai/fastai-v1-examples/fastai1-simpsons/train_sweep.py
@@ -32,7 +32,29 @@ if not (PATH_DATA).exists():
     r = requests.get(URL_DATA)
     PATH_DATAFILE.open("wb").write(r.content)
     with tarfile.open(PATH_DATAFILE) as archive:
-        archive.extractall('data')
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(archive, "data")
     PATH_DATAFILE.unlink()
 
 # Load data
