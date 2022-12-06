5095bc1977bed843319b26a2c1bce7cfc6bc11ec
Dmitry Duev <dmitryduev@users.noreply.github.com>
2022-12-06 07:45:44 -0800

    chore: update xgboost callback (#338)

--- examples/boosting-algorithms/xgboost-housing/train.py
+++ examples/boosting-algorithms/xgboost-housing/train.py
@@ -13,22 +13,26 @@ wandb.init(project="xgboost-housing-demo")
 # load data
 housing = fetch_california_housing()
 data = pd.DataFrame(housing.data)
-X, y = data.iloc[:,:-1],data.iloc[:,-1]
-data_dmatrix = xgb.DMatrix(data=X,label=y)
+X, y = data.iloc[:, :-1], data.iloc[:, -1]
+data_dmatrix = xgb.DMatrix(data=X, label=y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=123
+)
 
-xg_reg = xgb.XGBRegressor(objective ='reg:linear', 
-    colsample_bytree = 0.3, 
-    learning_rate = 0.1,
-    max_depth = 5, 
-    alpha = 10, 
-    n_estimators = 10)
+xg_reg = xgb.XGBRegressor(
+    objective="reg:linear",
+    colsample_bytree=0.3,
+    learning_rate=0.1,
+    max_depth=5,
+    alpha=10,
+    n_estimators=10,
+)
 
-xg_reg.fit(X_train,y_train,callbacks=[wandb.xgboost.wandb_callback()])
+xg_reg.fit(X_train, y_train, callbacks=[wandb.xgboost.WandbCallback()])
 
 preds = xg_reg.predict(X_test)
 
 rmse = np.sqrt(mean_squared_error(y_test, preds))
-print("RMSE: %f" % (rmse))
+print("RMSE: %f" % rmse)
 wandb.log({"RMSE": rmse})
