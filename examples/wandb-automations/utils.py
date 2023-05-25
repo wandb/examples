afb18c12a59db3c05f24be88ec5ff1567c30a556
Carey Phelps <carey@wandb.com>
2023-05-25 13:47:54 -0700

    new registered model name

--- examples/wandb-automations/utils.py
+++ examples/wandb-automations/utils.py
@@ -29,7 +29,7 @@ def save_model(model, model_name, models_folder="models", metadata=None, link=Fa
     model_artifact.add_file(file_name)
     wandb.log_artifact(model_artifact)
     if link:
-        wandb.run.link_artifact(model_artifact, 'model-registry/MNIST')
+        wandb.run.link_artifact(model_artifact, 'model-registry/MNIST Classifier')
 
 def load_model(model_artifact_name, eval=True):
     """Load the model from wandb artifacts
